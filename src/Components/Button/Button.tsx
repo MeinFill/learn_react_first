c445748bc5aa8ccf9bf2a107794839c011b85c05
Daniil <pers0nann@yandex.ru>
2025-09-17 14:00:02 +0500

    build the app

--- src/Components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import './button.css'
-function Button({text, className}: {text: string, className?: string}) {
-
-  return (
-      <button className={`button accent ${className}`}>{text}</button>
-  )
-}
-
-export default Button
